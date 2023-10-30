b0dce787e624376151cdec7f814beb0a2957c88b
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2023-10-30 23:34:52 +0530

    add a middleware and function to handle errors

--- backend/index.js
+++ backend/index.js
@@ -17,3 +17,15 @@ app.listen(3000,()=>{
 
 app.use("/backend/user",userRoutes);
 app.use("/backend/auth",authRoutes);
+
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode ||500; 
+    const message=err.message ||'Internal Server Error';
+    return res.status(statusCode).json({
+        sucess:false,
+        error:message,
+        statusCode
+    });
+}
+)
